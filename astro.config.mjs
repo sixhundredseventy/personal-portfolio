8cc22caeee1b74575d50985a9eac4adfb11e9838
sixhundredseventy <58562587+sixhundredseventy@users.noreply.github.com>
2024-11-12 13:09:15 +0100

    Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,9 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
-import cloudflare from '@astrojs/cloudflare';
-
 import compressor from 'astro-compressor';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compressor()],
-  output: 'server',
-  adapter: cloudflare()
 });
